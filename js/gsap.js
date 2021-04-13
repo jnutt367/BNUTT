1d5802170d8d606cac33d676e4bae44a86366c8a
Jason Nutt <jnutt367@gmail.com>
2021-04-13 16:26:46 -0400

    just messing around

---
+++ js/gsap.js
@@ -0,0 +1,5 @@
+gsap.fromTo("#wizard", { opacity: "0" }, { duration: 5, opacity: "1", ease: "inOut" });
+
+gsap.fromTo("#wiz-bubble", { x: -60, y: -150, opacity: "0" }, { duration: 3, x: -90, y: -150, opacity: "1" });
+
+gsap.fromTo("#g-bubble", { y: -200, opacity: "0" }, { duration: 30, y: -200, opacity: "1" });
