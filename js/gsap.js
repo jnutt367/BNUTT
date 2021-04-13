c72d84858ab1f2244abcc76f8294790ea78343e4
Jason Nutt <jnutt367@gmail.com>
2021-04-13 16:35:16 -0400

    commit big giant bubble effect

--- js/gsap.js
+++ js/gsap.js
@@ -2,4 +2,4 @@ gsap.fromTo("#wizard", { opacity: "0" }, { duration: 5, opacity: "1", ease: "inO
 
 gsap.fromTo("#wiz-bubble", { x: -60, y: -150, opacity: "0" }, { duration: 3, x: -90, y: -150, opacity: "1" });
 
-gsap.fromTo("#g-bubble", { y: -200, opacity: "0" }, { duration: 30, y: -200, opacity: "1" });
+gsap.fromTo("#g-bubble", { y: -200, opacity: "0" }, { duration: 10, x: -70, y: -240, scale: "2.5", opacity: "1" });
