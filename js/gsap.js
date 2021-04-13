1fcabdd3b11f8d514542d079df35b22c1ebf26a6
Jason Nutt <jnutt367@gmail.com>
2021-04-13 16:50:16 -0400

    commit newest wizard animation

--- js/gsap.js
+++ js/gsap.js
@@ -1,5 +1,7 @@
-gsap.fromTo("#wizard", { opacity: "0" }, { duration: 5, opacity: "1", ease: "inOut" });
+gsap.fromTo("#wizard", { x: 50, opacity: "0" }, { duration: 5, opacity: "1", ease: "inOut" });
 
-gsap.fromTo("#wiz-bubble", { x: -60, y: -150, opacity: "0" }, { duration: 3, x: -90, y: -150, opacity: "1" });
-
+gsap.fromTo("#wiz-bubble", { y: -150, opacity: "0" }, { duration: 3, x: 10, y: -150, opacity: "1" });
+gsap.fromTo("#wiz-bubble", { opacity: "1" }, { duration: 7, opacity: "0" });
 gsap.fromTo("#g-bubble", { y: -200, opacity: "0" }, { duration: 10, x: -70, y: -240, scale: "2.5", opacity: "1" });
+
+gsap.to("body", { duration: 1, scale: "1.2" })
